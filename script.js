d52725dfe3c687c88de75512b196e294f5386805
Catherine Pedersen <cathy.pedersen@gmail.com>
2019-11-19 16:21:51 -0500

    Latest

--- script.js
+++ script.js
@@ -12,41 +12,42 @@ function listenStartButton() {
         $('.eleanorQuoteImg').hide();
         $('#dark-money').removeClass('black-box');
         $('#dark-money').removeClass('ask-question');
+        //display quiz
         renderQuestionAndOptions();
         renderScorecard();
     });
 }
 
+//no longer using
 function listenRadioButton() {
     console.log("Listening for Radio button click");
     $('body').on('click', '#option', function (event) {
-        //DEBUG - need the index for the answer, not the text
+        //TODO - would like to use the index for the answer, not the text
         const answerText = $("input[type='radio']:checked").val();
         //const answers = $("#quiz-form input:radio[name='option']");
         //console.log(JSON.stringify(answers));
         //console.log("answers = " + answers);
         //const answer = answers.index(answers) 
         console.log("answerText = " + answerText);
-        renderAnswer(answerText);
-        renderScorecard();
-        console.log("answer = " + answerText);
+        //renderScorecard();
+        //console.log("answer = " + answerText);
         return answerText;
     });
-    //DEBUG
     //if (!answer) {
     //    alert("Please choose an option");
-    //    return;
+    //return;
     //}
 }
 
 function listenSubmitButton() {
     console.log("Listening for Submit button click");
-    $('body').on('click', '.submit', function (event) {
-        //don't try to submit to a server
+    $('body').on('click', '#submit', function (event) {
+        //don't submit to a server
         event.preventDefault();
         //listen for user selection of an answer
-        const answer = listenRadioButton();
-        handleAnswer(answer);
+        const answer = $("input[type='radio']:checked").val();
+        console.log("answer = " + answer);
+        renderAnswer(answer);
         renderScorecard();
     });
 }
@@ -111,58 +112,84 @@ function renderNewQuestionAndOptions() {
     console.log("optionsArr = " + optionsArr);
 
     //create question/options quiz form
+    //DEBUG3 - required isn't working
     const formHtml = $(`
         <form id="quiz-form" class="quiz-form">
             <ul style="list-style-type:none;">
-                <li><input type="radio" name="options" id="option" value="${optionsArr[0]}">${optionsArr[0]}</li>
-                <li><input type="radio" name="options" id="option" value="${optionsArr[1]}">${optionsArr[1]}</li>
-                <li><input type="radio" name="options" id="option" value="${optionsArr[2]}">${optionsArr[2]}</li>
-                <li><input type="radio" name="options" id="option" value="${optionsArr[3]}">${optionsArr[3]}</li>
+                <li><input type="radio" name="options" id="option" value="${optionsArr[0]}"
+                required>${optionsArr[0]}</li>
+                <li><input type="radio" name="options" id="option" value="${optionsArr[1]}"
+                required>${optionsArr[1]}</li>
+                <li><input type="radio" name="options" id="option" value="${optionsArr[2]}"
+                required>${optionsArr[2]}</li>
+                <li><input type="radio" name="options" id="option" value="${optionsArr[3]}"
+                required>${optionsArr[3]}</li>
             </ul>
+            <button id="submit" type="submit" class="button">Submit</button>
         </form>
     `);
 
+    //$('.button').html("Submit");
+    //$('.button').addClass("submit");
+    //return buttonHtml;
+    //$('#dark-money').append(buttonHtml);
+    //DEBUG1 - Various Submit button problems
+    //display the submit button
+    //buttonHtml = renderButton('submit');
+    //$(buttonHtml).appendTo(formHtml);
+    
     //insert html into the dom
     $('h2').html(questionText);
     $('#select-answer').html("Select the correct answer:");
     $('#possible-answers').html(formHtml);
-
-    //display the submit button
-    renderButton('submit');
 
     //increment questionIndex
     DATA['questionIndex'] = DATA.questionIndex + 1; 
 }
 
 function renderButton(option) {
+    //DEBUG2 - render button issues
     console.log("Rendering button: " + option);
     if (option === "submit") {
+        //$('#start').hide('.start');
         const buttonHtml = $(`
-            <button id="submit" type="button" class="button">Submit</button>
-        `);
-        $('.button').html("Submit");
+        <button id="submit" type="submit" class="button">Submit</button>
+    `);
+        //$('.button').html("Submit");
         //$('.button').addClass("submit");
+        //return buttonHtml;
+        $('#dark-money').append(buttonHtml);
     } else if (option === "next") {
         const buttonHtml = $(`
             <button id="next" type="button" class="button">Next</button>
         `);
-        $('.button').html("Next");
+        //$('#start').show('.start');
+        //$('.button').html("Next");
         //$('.button').addClass("next");
+        $('#button').hide();
+        $('#dark-money').append(buttonHtml);
     } else if (option === "start") {
         const buttonHtml = $(`
             <button id="start" type="button" class="button">Start</button>
         `);
-        $('.button').html("Start");
+        //$('#start').show('.start');
+        //$('.button').html("Start");
         //$('.button').addClass("start");
+        $('#button').hide();
+        $('#dark-money').append(buttonHtml);
     } else if (option === "restart") {
         const buttonHtml = $(`
             <button id="restart" type="button" class="button">Restart</button>
         `);
-        $('.button').html("Restart");
+        //$('#start').show('.start');
+        //$('.button').html("Restart");
         //$('.button').addClass("restart");
+        $('#button').hide();
+        $('#dark-money').append(buttonHtml);
     }
     //$('#button').empty();
     //$('.button').html(buttonHtml);
+    
 }
 
 function renderAnswer() {
@@ -171,7 +198,7 @@ function renderAnswer() {
     //call handleAnswer to get pass or fail
 
     //call either renderCorrectAnswer or renderWrongAnswer
-    //renderButton('next');
+    renderButton('next');
 }
 
 function renderCorrectAnswer(indexAnswer) {
@@ -222,11 +249,10 @@ function renderWonResult() {
 //********************************************************* */
 function handleAnswer(answer) {
     console.log("Handling the selected answer");
-
-
     console.log("number of questions = " + DATA.questions.length);
 
     //compare answer to expected answer
+    //result = 0 or 1
 
     //update DATA object with result
 
