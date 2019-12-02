e62b924fb20a9e3e4066280623617ba6011c1f9e
Catherine Pedersen <cathy.pedersen@gmail.com>
2019-12-01 23:47:13 -0500

    Getting there

--- data.js
+++ data.js
@@ -27,34 +27,66 @@ const TEST = {
     questionNum: 1,
     questions: [
         {
-            question: "Question 1",
-            options: ["Answer A", "Answer B", "Answer C", "Answer D"],
-            correctAnswer: ["Answer C"]
+            question: "What is \"dark money\"?",
+            options: ["Campaign money whose sources are not disclosed.", "Taxes paid anonymously by corporate interests.", "Money funneled via super PACs (political action committees) to billionaries.", "Oil money used to fight climate change."],
+            correctAnswer: ["Campaign money whose sources are not disclosed."]
         },
 
         {
-            question: "Question 2",
-            options: ["Answer A", "Answer B", "Answer C", "Answer D"],
-            correctAnswer: ["Answer A"]
+            question: "Why is dark money a problem in the U.S.?",
+            options: ["It prevents anonymous donations to political campaigns.", "It makes it harder to detect violations of campaign finance law.", "It keeps voters from the polls.", "It gives voters access to information on who is influencing candidates for political office."],
+            correctAnswer: ["It makes it harder to detect violations of campaign finance law."]
+        },
+
+
+        {
+            question: "Which sector contributes the most dark money?",
+            options: ["Finance/Insurance/Real Estate", "Health", "Labor", "Single Issue or Ideology Organizations"],
+            correctAnswer: ["Finance/Insurance/Real Estate"]
         },
 
         {
-            question: "Question 3",
-            options: ["Answer A", "Answer B", "Answer C", "Answer D"],
-            correctAnswer: ["Answer D"]
+            question: "Which Supreme Court decision led to the rise of dark money?",
+            options: ["In United States v. Harriss (1954), the Supreme Court ruled that the Regulation of Lobbying Act was constitutional.", "In Citizens United v. Federal Election Commission (2010), the Supreme Court ruled that the free speech clause of the First Amendment prohibited the government from restricting corporation and union spending on political campaigns.", "In SpeechNow.org v. FEC (2010), the U.S. District Court for the District of Columbia ruled that the FEC could no longer enforce contribution limits to independent groups.", "All of the above."],
+            correctAnswer: ["In Citizens United v. Federal Election Commission (2010), the Supreme Court ruled that the free speech clause of the First Amendment prohibited the government from restricting corporation and union spending on political campaigns."]
         },
 
         {
-            question: "Question 4",
-            options: ["Answer A", "Answer B", "Answer C", "Answer D"],
-            correctAnswer: ["Answer B"]
+            question: "How has dark money shaped our judicial system?",
+            options: ["There is a significant relationship between business group contributions to state supreme court justices and the voting of those justices.", "Campaign donations from the business community appeared to have a greater effect on rulings by Democratic judges.", "The North Carolina Supreme Court has not sided with environmental interests over business interests for over 15 years.", "All of the above."],
+            correctAnswer: ["All of the above."]
         },
 
         {
-            question: "Question 5",
-            options: ["Answer A", "Answer B", "Answer C", "Answer D"],
-            correctAnswer: ["Answer A"]
+            question: "What made the 2014 election cycle special?",
+            options: ["The Koch brothers bought half of the ads.", "Liberal dark money spending caught up to conservative spending.", "It was the highest amount of dark money spent in a congressional election to date.", "All of the above."],
+            correctAnswer: ["It was the highest amount of dark money spent in a congressional election to date."]
+        },
+
+        {
+            question: "What made the 2018 election cycle special?",
+            options: ["Liberal dark money outspent conservative spending.", "Total amount spent reached 1 billion.", "Liberal groups began spending through less apparent channels.", "All of the above."],
+            correctAnswer: ["Liberal dark money outspent conservative spending."]
+        },
+
+        {
+            question: "What makes super PACs so special?",
+            options: ["They enable candidates to bypass prohibitions in taking money from corporations or unions.", "They can accept unlimited contributions and contribute unlimited funds.", "They must publicly disclose their list of donors, while dark money groups have no such legal obligation.", "All of the above."],
+            correctAnswer: ["All of the above."]
+        },
+
+        {
+            question: "How is campaign spending and political success linked?",
+            options: ["Money isn’t the deciding factor in elections.", "Large amounts of spending leads to wealthier candidates running and winning elections.", "Advertising is most effective if the candidate is not already well-known and if the election is not pre-determined along party lines.", "All of the above."],
+            correctAnswer: ["All of the above."]
+        },
+
+        {
+            question: "What is the best way to fight dark money?",
+            options: ["A constitutional amendment to overturn Citizens United.", "A presidential executive order to expose secret political spending by federal contractors.", "An IRS move to revoke the tax exemption status of dark-money groups.", "All of the above."],
+            correctAnswer: ["All of the above."]
         }
+
     ]
 };
 
